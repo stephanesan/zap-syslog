6974c7559ba2078c291bcd9c3eeb4339017743f5
stlejeun <stlejeun@cisco.com>
2020-07-24 18:29:05 +0200

    Fix local repo imports

--- encoder.go
+++ encoder.go
@@ -26,9 +26,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/imperfectgo/zap-syslog/internal"
-	"github.com/imperfectgo/zap-syslog/internal/bufferpool"
-	"github.com/imperfectgo/zap-syslog/syslog"
+	"github.com/stephanesan/zap-syslog/internal"
+	"github.com/stephanesan/zap-syslog/internal/bufferpool"
+	"github.com/stephanesan/zap-syslog/syslog"
 	"go.uber.org/zap"
 	"go.uber.org/zap/buffer"
 	"go.uber.org/zap/zapcore"
